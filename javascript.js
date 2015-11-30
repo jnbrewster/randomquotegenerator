ab34f54219afa96957cb943dcf237abb2285c89e
Joel <j.n.brewster@gmail.com>
2015-11-30 12:46:02 +0900

    finished functions, need to add styling and content

--- javascript.js
+++ javascript.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-    generate();
 
-    function getQuotes() {
-        var quotes = ["Oooooooh!", "Podo!", "Dohdoh!"];
-        var authors = ["-Leon", "-Leon", "-Leon"];
+    function getQuote() {
+        var quotes = ["Oooooooh!", "Podo!", "Dohdoh!", "Gyul!", "Dada!", "Mama!", "Omma!"];
+        var authors = ["-Leon", "-Leon", "-Leon", "-Leon", "-Leon", "-Leon", "-Leon"];
 
-        randomNum = quotes[Math.floor(Math.random()*quotes.length)];
+        var randomNum = Math.floor(Math.random()*quotes.length);
 
         var randomQuote = quotes[randomNum];
         var corresAuthor = authors[randomNum];
 
+        $('.quote').text(randomQuote);
+        $('.author').text(corresAuthor);
 
       }
 
-    $(".button").on( "click", function() {
-    generate();
+    $(".btn").on("click", function() {
+    getQuote();
     });
 });
+
+
